f51bac1079a6efbeefb65d3bc321c0ec0227e8f0
Dylan Hand <dhand@galois.com>
2020-05-19 08:06:28 +0000

    Update chipyard to latest. Update README to reflect new capabilities. Update sample configs to match latest pre-built images and options

---
+++ gfe_fixes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This file should be run after setting up your firesim manager instance. It modifies
+# some files deep down the submodule hierarchy to avoid having to fork the repos
+
+function die() {
+	echo "Could not find riscv-pk files. Did you fully setup your manager instance?"
+	exit -1
+}	
+
+if [ -f target-design/chipyard/software/firemarshal/riscv-pk/bbl/bbl.lds ]
+then
+	echo "Patching riscv-pk/bbl/bbl.lds"
+	sed -i 's/0x80000000/0xc0000000/' target-design/chipyard/software/firemarshal/riscv-pk/bbl/bbl.lds
+else
+	die
+fi
+
+if [ -f target-design/chipyard/software/firemarshal/riscv-pk/machine/uart.c ]
+then
+	echo "Patching riscv-pk/machine/uart.c"
+	sed -i 's/= 868/= 27/' target-design/chipyard/software/firemarshal/riscv-pk/machine/uart.c
+else
+	die
+fi
+
+echo "Done."
