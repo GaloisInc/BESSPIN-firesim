e06d76fec947f7b12e0d1d5bb6ce3a7f1fb4b372
Dylan Hand <dhand@galois.com>
2020-05-27 23:12:10 -0700

    Add AWS credential passthrough for docker

--- start_docker.sh
+++ start_docker.sh
@@ -13,5 +13,10 @@ if [ ! -z ${SSH_AUTH_SOCK+x} ]
 then
   DOCKER_OPTS+=(-v $(readlink -f $SSH_AUTH_SOCK):/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent)
 fi
+if [ ! -z ${AWS_ACCESS_KEY_ID+z} ]
+then
+  # Assume the other keys are set too
+  DOCKER_OPTS+=(-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN")
+fi
 
 sudo docker run "${DOCKER_OPTS[@]}" $DOCKER_CONTAINER bash
