a24bbf7ddc14b724bdd2df95d8ba9cfcfd2a768a
Dylan Hand <dhand@galois.com>
2020-06-01 16:08:20 -0700

    Passthrough AWS default region to docker

--- start_docker.sh
+++ start_docker.sh
@@ -16,7 +16,13 @@ fi
 if [ ! -z ${AWS_ACCESS_KEY_ID+z} ]
 then
   # Assume the other keys are set too
-  DOCKER_OPTS+=(-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN")
+  # But check for the region
+  if [ -z ${AWS_DEFAULT_REGION+x} ]
+  then
+     AWS_DEFAULT_REGION="us-west-2"
+     echo "WARN: Setting default AWS region to \"$AWS_DEFAULT_REGION\". If this is incorrect, set the correct region in AWS_DEFAULT_REGION"
+  fi
+  DOCKER_OPTS+=(-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN" -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION)
 fi
 
 sudo docker run "${DOCKER_OPTS[@]}" $DOCKER_CONTAINER bash
